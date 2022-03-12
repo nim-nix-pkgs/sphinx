8a207e8ac5d5247fc87756a01838492e0269ee94
hugosenari <hugosenari@gmail.com>
2022-03-12 04:16:07 -0300

    chore: re index sphinx

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Sphinx wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."sphinx-master".url = "path:./master";
+  inputs."sphinx-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."sphinx-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
